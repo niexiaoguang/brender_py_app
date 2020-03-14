58d1b3d317ba116a3b334f903141c3a06b11e0ad
niexiaoguang <nxg@lohosoft.com>
2020-03-14 10:01:01 +0800

    update run command with port map,5678 is amqp.brender.cn without ssl but with nginx filter for ip

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ build :
 docker build -t py_dev --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)  -f Dockerfile.py.dev  .
 
 dev run :
-docker run --rm -u $(id -u):$(id -g) -it --expose=5671 -p 9000:9000  -v $(pwd):/usr/app py_dev bash
+docker run --rm -u $(id -u):$(id -g) -it -p 5678:5678  -v $(pwd):/usr/app py_dev bash
--- src/index.py
+++ src/index.py
@@ -1,7 +1,7 @@
-import pika
 import os
 import sys
 import ssl
+import pika
 from pika.credentials import ExternalCredentials
 
 
@@ -19,131 +19,191 @@ print(args)
 def start():
     print("i'm index file haha")
 
-name = args[0]
-
-ssl_options = {    
-    "ca_certs":"./ssl/cacert.pem",
-    "certfile": "./ssl/client/rabbit-client.cert.pem",
-    "keyfile": "./ssl/client/rabbit-client.key.pem",
-    "cert_reqs": ssl.CERT_REQUIRED,
-    "ssl_version":ssl.PROTOCOL_TLSv1_2
-}
-
-def connect():
-    # credentials = pika.PlainCredentials('test', 'test')
-    # parameters = pika.ConnectionParameters(host=amqp_host,
-    #                                        port=5671,
-    #                                        virtual_host='/',
-    #                                        heartbeat_interval = 30,
-    #                                        credentials=credentials,
-    #                                        ssl = True,
-    #                                        ssl_options = ssl_options)
-    # connection = pika.BlockingConnection(parameters)
-    # print(connection)
-    # connection.close()
+# name = args[0]
 
 
+# ssl_options = dict(    
 
-    # context = ssl.create_default_context(cafile="./ssl/cacert.pem")
-    # context.load_cert_chain(certfile="./ssl/client/rabbit-client.cert.pem",keyfile="./ssl/client/rabbit-client.key.pem",password="PLE427VKgNSpqEXN")
-    # ssl_options = pika.SSLOptions(context, amqp_host)
-    # conn_params = pika.ConnectionParameters(port=5671,ssl_options=ssl_options)
+#     ca_certs="./ssl/cacert.pem",
+#     certfile="./ssl/client/rabbit-client.cert.pem",
+#     keyfile= "./ssl/client/rabbit-client.key.pem",
+#     cert_reqs= ssl.CERT_REQUIRED,
+#     password="PLE427VKgNSpqEXN",
+#     ssl_version=ssl.PROTOCOL_TLSv1_2
+# )
+# credentials = pika.PlainCredentials('guest', 'guest')
+# parameters = pika.ConnectionParameters(host='ampqs:amqp.brender.cn',
+#                                        port=5671,
+#                                        virtual_host='/',
+#                                        heartbeat_interval = 30,
+#                                        credentials=credentials,
+#                                        ssl = True,
+#                                        ssl_options = ssl_options)
+# connection = pika.BlockingConnection(parameters)
 
-    # with pika.BlockingConnection(conn_params) as conn:
-    #     ch = conn.channel()
-    #     ch.queue_declare("hello")
-    #     ch.basic_publish("", "hello", "Hello, world!")
-    #     print(ch.basic_get("hello"))
-
-
-
-
-    context = ssl.create_default_context(cafile="./ssl/cacert.pem")
-
-    # I know it's much better to use another certificate but sometimes there is no choise:
-    # I know it's very rude solution. Use it as start point
-
-    context.set_ciphers('ALL:@SECLEVEL=0') # 
-
-    context.load_cert_chain(certfile="./ssl/client/rabbit-client.cert.pem",keyfile="./ssl/client/rabbit-client.key.pem",password="PLE427VKgNSpqEXN")
-    # context.set_ciphers('ALL:@SECLEVEL=0')
-
-    ssl_options = pika.SSLOptions(context, amqp_host)
-    conn_params = pika.ConnectionParameters(host=amqp_host,
-                                            port=5671,
-                                            # virtual_host="/",
-                                            ssl_options=ssl_options,
-                                            credentials=ExternalCredentials())
+# print(connection)
+# connection.close()
 
 
+# context = ssl.create_default_context(cafile="./ssl/cacert.pem")
+# context.set_ciphers('ALL:@SECLEVEL=0') # 
 
-    with pika.BlockingConnection(conn_params) as conn:
-         ch = conn.channel()
-         ch.queue_declare("hello")
-         ch.basic_publish("", "hello", "Hello, world from python!")
-         print(ch.basic_get("hello"))
+# context.load_cert_chain(certfile="./ssl/client/rabbit-client.cert.pem",
+#                         keyfile="./ssl/client/rabbit-client.key.pem",
+#                         password="PLE427VKgNSpqEXN")
 
-
-
-    # cp = pika.ConnectionParameters(amqp_host, 5671, '/',
-    #                                 ssl=True,
-    #                                 ssl_options=dict(
-    #                                     ssl_version=ssl.PROTOCOL_TLSv1,
-    #                                     ca_certs="./ssl/cacert.pem",
-    #                                     keyfile="./ssl/client/rabbit-client.key.pem",
-    #                                     certfile="./ssl/client/rabbit-client.cert.pem",
-    #                                     cert_reqs=ssl.CERT_REQUIRED)
-    #                                 )
+# ssl_options = pika.SSLOptions(context, 'amqp.brender.cn')
+# conn_params = pika.ConnectionParameters(
+#                                         host='amqp.brender.cn',
+#                                         port=5671,
+#                                         ssl_options=ssl_options,
+#                                         credentials=ExternalCredentials(),
+#                                         heartbeat=30)
 
 
-    # with pika.BlockingConnection(cp) as conn:
-    #      ch = conn.channel()
-    #      ch.queue_declare("hello")
-    #      ch.basic_publish("", "hello", "Hello, world from python!")
-    #      print(ch.basic_get("hello"))
+# context = ssl.create_default_context()
+# context.load_cert_chain('./ssl/1_amqp.brender.cn_bundle.crt',
+#                         './ssl/2_amqp.brender.cn.key')
+
+# context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+# context.load_verify_locations('./ssl/1_amqp.brender.cn_bundle.crt')
 
 
-connect()
+# ssl_options = pika.SSLOptions(context, 'amqp.brender.cn')
+# conn_params = pika.ConnectionParameters(
+#                                         host='amqp.brender.cn',
+#                                         port=443,
+#                                         ssl_options=ssl_options,
+#                                         credentials=ExternalCredentials(),
+#                                         heartbeat=30)
+
+# with pika.BlockingConnection(conn_params) as conn:
+#     ch = conn.channel()
+#     ch.queue_declare("hello")
+#     ch.basic_publish("", "hello", "Hello, world from python!")
+#     print(ch.basic_get("hello"))
+#     input("Press Enter to continue...")
+
+credentials = pika.PlainCredentials("pata", "pprabbit")
 
 
-def send(name):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=amqp_host))
-    channel = connection.channel()
+conn_params = pika.ConnectionParameters(host='amqp.brender.cn',
+                                        port=5678,
+                                        virtual_host='/',
+                                        ssl_options=None,
+                                        credentials=credentials,
+                                        heartbeat=30)
 
-    channel.queue_declare(queue='hello')
-    channel.queue_declare(queue='hello1')
-
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello from ' + name)
-
-    channel.basic_publish(exchange='', routing_key='hello1', body='Hello1 from ' + name)
-    print(" [x] Sent 'Hello World!'")
-    connection.close()
-
-def recv():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=amqp_host))
-    channel = connection.channel()
-
-    channel.queue_declare(queue='hello')
-    channel.queue_declare(queue='hello1')
-
-
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
-
-    # def callback1(ch, method, properties, body):
-    #     print(" [x] Received1 %r" % body)
+with pika.BlockingConnection(conn_params) as conn:
+    ch = conn.channel()
+    ch.queue_declare("hello")
+    ch.basic_publish("", "hello", "Hello, world from python lalala!")
+    # print(ch.basic_get("hello"))
 
 
 
-    channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
 
-    # channel.basic_consume(
-        # queue='hello1', on_message_callback=callback1, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+
+
+
+
+
+
+# ssl_options = {    
+#     "ca_certs":"./ssl/cacert.pem",
+#     "certfile": "./ssl/client/rabbit-client.cert.pem",
+#     "keyfile": "./ssl/client/rabbit-client.key.pem",
+#     "cert_reqs": ssl.CERT_REQUIRED,
+#     "ssl_version":ssl.PROTOCOL_TLSv1_2
+# }
+
+# def connect1():
+#     # credentials = pika.PlainCredentials('test', 'test')
+#     # parameters = pika.ConnectionParameters(host=amqp_host,
+#     #                                        port=5671,
+#     #                                        virtual_host='/',
+#     #                                        heartbeat_interval = 30,
+#     #                                        credentials=credentials,
+#     #                                        ssl = True,
+#     #                                        ssl_options = ssl_options)
+#     # connection = pika.BlockingConnection(parameters)
+#     # print(connection)
+#     # connection.close()
+
+
+
+#     # context = ssl.create_default_context(cafile="./ssl/cacert.pem")
+#     # context.load_cert_chain(certfile="./ssl/client/rabbit-client.cert.pem",keyfile="./ssl/client/rabbit-client.key.pem",password="PLE427VKgNSpqEXN")
+#     # ssl_options = pika.SSLOptions(context, amqp_host)
+#     # conn_params = pika.ConnectionParameters(port=5671,ssl_options=ssl_options)
+
+#     # with pika.BlockingConnection(conn_params) as conn:
+#     #     ch = conn.channel()
+#     #     ch.queue_declare("hello")
+#     #     ch.basic_publish("", "hello", "Hello, world!")
+#     #     print(ch.basic_get("hello"))
+
+
+
+
+#     context = ssl.create_default_context(cafile="./ssl/cacert.pem")
+
+#     # I know it's much better to use another certificate but sometimes there is no choise:
+#     # I know it's very rude solution. Use it as start point
+
+#     context.set_ciphers('ALL:@SECLEVEL=0') # 
+
+#     context.load_cert_chain(certfile="./ssl/client/rabbit-client.cert.pem",keyfile="./ssl/client/rabbit-client.key.pem",password="PLE427VKgNSpqEXN")
+#     # context.set_ciphers('ALL:@SECLEVEL=0')
+
+#     ssl_options = pika.SSLOptions(context, amqp_host)
+#     conn_params = pika.ConnectionParameters(host=amqp_host,
+#                                             port=5671,
+#                                             # virtual_host="/",
+#                                             ssl_options=ssl_options,
+#                                             credentials=ExternalCredentials())
+
+
+
+# def send(name):
+#     connection = pika.BlockingConnection(
+#         pika.ConnectionParameters(host=amqp_host))
+#     channel = connection.channel()
+
+#     channel.queue_declare(queue='hello')
+#     channel.queue_declare(queue='hello1')
+
+#     channel.basic_publish(exchange='', routing_key='hello', body='Hello from ' + name)
+
+#     channel.basic_publish(exchange='', routing_key='hello1', body='Hello1 from ' + name)
+#     print(" [x] Sent 'Hello World!'")
+#     connection.close()
+
+# def recv():
+#     connection = pika.BlockingConnection(
+#         pika.ConnectionParameters(host=amqp_host))
+#     channel = connection.channel()
+
+#     channel.queue_declare(queue='hello')
+#     channel.queue_declare(queue='hello1')
+
+
+#     def callback(ch, method, properties, body):
+#         print(" [x] Received %r" % body)
+
+#     # def callback1(ch, method, properties, body):
+#     #     print(" [x] Received1 %r" % body)
+
+
+
+#     channel.basic_consume(
+#         queue='hello', on_message_callback=callback, auto_ack=True)
+
+#     # channel.basic_consume(
+#         # queue='hello1', on_message_callback=callback1, auto_ack=True)
+
+#     print(' [*] Waiting for messages. To exit press CTRL+C')
+#     channel.start_consuming()
 
 
