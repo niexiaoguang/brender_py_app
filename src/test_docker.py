dedc3b185f19337f0fdf5ea2683ca62d66981561
niexiaoguang <nxg@lohosoft.com>
2020-03-30 15:25:59 +0800

    update ignore

--- src/test_docker.py
+++
@@ -1,6 +0,0 @@
-import docker
-# client = docker.from_env()
-client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-
-# client.containers.run("alpine", ["echo", "hello", "world"])
-print(client.containers.list())   
