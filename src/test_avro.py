c9ae69048224ecbb6aacc8154b0bafb3532d0a80
niexiaoguang <nxg@lohosoft.com>
2020-04-01 11:47:14 +0800

    pyinstaller package need avro files

---
+++ src/test_avro.py
@@ -0,0 +1,45 @@
+import sys
+lib_path = './avro-python3-1.9.2'
+sys.path.append(lib_path)
+import avro
+import io
+import avro.schema
+import avro.io
+
+# mayby installer need it ? TODO
+# import json
+
+
+test_schema = '''
+{
+"namespace": "example.avro",
+ "type": "record",
+ "name": "User",
+ "fields": [
+     {"name": "sname", "type": "string"},
+     {"name": "favorite_number",  "type": ["int", "null"]},
+     {"name": "favorite_color", "type": ["string", "null"]}
+ ]
+}
+'''
+
+schema = avro.schema.Parse(test_schema)
+writer = avro.io.DatumWriter(schema)
+
+bytes_writer = io.BytesIO()
+encoder = avro.io.BinaryEncoder(bytes_writer)
+writer.write({"sname": "Alyssa", "favorite_number": 256}, encoder)
+writer.write({"sname": "Ben", "favorite_number": 7, "favorite_color": "red"}, encoder)
+
+raw_bytes = bytes_writer.getvalue()
+print(len(raw_bytes))
+print(type(raw_bytes))
+
+bytes_reader = io.BytesIO(raw_bytes)
+decoder = avro.io.BinaryDecoder(bytes_reader)
+reader = avro.io.DatumReader(schema)
+user1 = reader.read(decoder)
+user2 = reader.read(decoder)
+
+print(user2['sname'])
+print(type(user1['favorite_number']))
